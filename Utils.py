c795fb1951e925185d8b56792fb6fa9b1fe1d6df
samlerman <slerman@ur.rochester.edu>
2022-01-15 20:33:21 -0500

    debug

--- Utils.py
+++ Utils.py
@@ -48,7 +48,7 @@ def load(path, module):
             else:
                 warnings.warn(f'Load path {path} does not exist.')
             fetch = False
-        except (RuntimeError, EOFError):
+        except:
             warnings.warn(f'Load conflict')
             pass
 
